7b59960027ae9fa1c72a6400f4bacca35fc202fd
BogyTrap <zagorodniybogdan2@gmail.com>
2021-07-10 15:17:05 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,34 @@ $(function() {
         slidesToScroll: 4
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function() {
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+
+    $('.icon-th-large').on('click', function() {
+        $('.product__item').removeClass('list');
+        $('.icon-th-list').removeClass('active');
+        $('.icon-th-large').addClass('active');
+    });
+
+    $('.header__bottom-menu-btn').on('click', function() {
+        $('.header__bottom-menu-list').slideToggle();
+    });
+    $('.header__btn-menu').on('click', function() {
+        $('.header__top-inner-box').toggleClass('active');
+    });
+
     var mixer = mixitup('.products__inner-box');
 });
