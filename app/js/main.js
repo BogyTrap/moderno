acf9805a6787382fe07dc4e8b0cebeb7080b22e1
BogyTrap <zagorodniybogdan2@gmail.com>
2021-07-20 17:28:45 +0300

    added 404, about, contact

--- app/js/main.js
+++ app/js/main.js
@@ -36,6 +36,15 @@ $(function() {
         $('.icon-th-large').addClass('active');
     });
 
+    $('.solo-product__content-tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.solo-product__content-tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.solo-product__content-tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
     $('.header__bottom-menu-btn').on('click', function() {
         $('.header__bottom-menu-list').slideToggle();
     });
