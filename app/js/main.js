6b3dc48e866dff57568e4a40932bd485b4017b3f
BogyTrap <zagorodniybogdan2@gmail.com>
2021-07-21 14:29:53 +0300

    added settings page

--- app/js/main.js
+++ app/js/main.js
@@ -36,10 +36,10 @@ $(function() {
         $('.icon-th-large').addClass('active');
     });
 
-    $('.solo-product__content-tabs .tab').on('click', function(event) {
+    $('.solo-product__content-tabs .tab, .settings__tabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
-        $('.solo-product__content-tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.solo-product__content-tabs .tabs').find('.tab').removeClass('active');
+        $('.solo-product__content-tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.solo-product__content-tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
